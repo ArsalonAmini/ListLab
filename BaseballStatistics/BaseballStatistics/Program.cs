e70c1c0d593c571bccfff998698ce1cae696a274
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-10 16:16:57 -0500

    Baseball statistics list completed 8/10/2016 by A.Amini-Hajibashi

---
+++ BaseballStatistics/BaseballStatistics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> baseballstatistics = new List<int>();
+            baseballstatistics.Add(6);
+            baseballstatistics.Add(7);
+            baseballstatistics.Add(8);
+
+            List<string> baseballstatisticnames = new List<string>();
+            baseballstatisticnames.Add("John");
+            baseballstatisticnames.Add("Roger");
+            baseballstatisticnames.Add("Gina");
+
+            for (int i=0; i<baseballstatistics.Count; i++)
+            {
+                Console.WriteLine(baseballstatisticnames[i]+ " " + baseballstatistics[i]);   
+            }
+            Console.ReadLine();
+        }
+
+       }
+}
+
---
+++ Baseballstatisticsv2/Baseballstatisticsv2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baseballstatisticsv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Statistics> baseBall = new List<Statistics>{
+            new Statistics{amount = 6.8, type="M.Trout"},
+            new Statistics{amount = 6.7, type="J.Altuve"};
+            new Statistics {amount = 5.6, type = "J.Donaldson"}; 
+       class Statistics
+        {
+            public int amount { get; set; }
+            public string type { get; set; }
+        }
+
+    }
